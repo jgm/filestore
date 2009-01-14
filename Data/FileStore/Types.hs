235a5cdc84184aa9efaa0d9731d0cdec347e2808
jgm <jgm@berkeley.edu>
2009-01-14 08:35:10 -0800

    Changed Data.FileStore.Types to use records instead of type classes.
    Ignore-this: bd1935e10bcacad4a0b42f856e15a10f
    
    darcs-hash:20090114163510-0f649-49217674f1eb7f23e17e9524ba2f643f5f18f49b.gz

--- Data/FileStore/Types.hs
+++ Data/FileStore/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeSynonymInstances, DeriveDataTypeable #-}
+{-# LANGUAGE Rank2Types, TypeSynonymInstances, DeriveDataTypeable #-}
 {- |
    Module      : Data.FileStore.Types
    Copyright   : Copyright (C) 2008 John MacFarlane
@@ -21,19 +21,20 @@ module Data.FileStore.Types
            , TimeRange(..)
            , MergeInfo(..)
            , FileStoreError(..)
-           , FileStoreInfo(..)
            , SearchMatch(..)
            , SearchQuery(..)
            , defaultSearchQuery
            , DateTime
            , FileStore (..)
-           , SearchableFileStore (..) )
+           , diff
+           , modify
+           , create )
 
 where
 import Data.ByteString.Lazy (ByteString)
+import Data.Typeable
 import Data.ByteString.Lazy.UTF8 (toString, fromString)
 import Data.DateTime (DateTime)
-import Data.Typeable
 import Control.Exception (Exception, throwIO, catch, SomeException)
 import Data.FileStore.Utils (mergeContents, diffContents)
 import Prelude hiding (catch)
@@ -46,7 +47,7 @@ data Author =
   Author {
     authorName  :: String
   , authorEmail :: String
-  } deriving (Show, Read, Eq)
+  } deriving (Show, Read, Eq, Typeable)
 
 data Change =
     Added ResourceName
@@ -79,7 +80,7 @@ data TimeRange =
   TimeRange {
     timeFrom :: Maybe DateTime  -- ^ @Nothing@ means no lower bound
   , timeTo   :: Maybe DateTime  -- ^ @Nothing@ means no upper bound
-  } deriving (Show, Read, Eq)
+  } deriving (Show, Read, Eq, Typeable)
 
 data MergeInfo =
   MergeInfo {
@@ -106,7 +107,7 @@ data SearchQuery =
   , queryWholeWords  :: Bool     -- ^ Match patterns only with whole words?
   , queryMatchAll    :: Bool     -- ^ Return matches only from files in which all patterns match?
   , queryIgnoreCase  :: Bool     -- ^ Make matches case-insensitive?
-  } deriving (Show, Read, Eq)
+  } deriving (Show, Read, Eq, Typeable)
 
 defaultSearchQuery :: SearchQuery
 defaultSearchQuery = SearchQuery {
@@ -121,78 +122,44 @@ data SearchMatch =
     matchResourceName :: ResourceName
   , matchLineNumber   :: Integer
   , matchLine         :: String
-  } deriving (Show, Read, Eq)
+  } deriving (Show, Read, Eq, Typeable)
 
-data FileStoreInfo =
-  FileStoreInfo {
-      fileStoreType     :: String
-    , fileStorePath     :: Maybe FilePath
-    } deriving (Show, Read, Eq)
+-- | A versioning filestore, which can be implemented using the
+-- file system, a database, or revision-control software.
+data FileStore = FileStore {
 
--- | An abstract class for a versioning filestore, which can be
--- implemented using the file system, a database, or revision-control
--- software. A minimal instance definition will define 'fileStoreInfo', 'initialize',
--- 'save', 'retrieve', 'delete', 'rename', 'history', 'latest', 'revision',
--- and 'index'. Sensible defaults are provided for 'modify', 'create',
--- 'diff', and 'idsMatch', so these normally do not need to be
--- implemented.
-class FileStore b where
-
-    -- | Return information about the filestore.
-    fileStoreInfo  :: b
-                   -> IO FileStoreInfo
+    -- | Return the type of the filestore (e.g. "Git")
+    fsType         :: IO String
+   
+    -- | Return @Just@ the path of the filestore, if it is implemented
+    -- in the filesystem, or @Nothing@ otherwise.
+  , fsPath         :: IO (Maybe FilePath)
 
     -- | Initialize a new filestore.
-    initialize     :: b
-                   -> IO ()
+  , initialize     :: IO ()
 
     -- | Save contents in the filestore.
-    save           :: Contents a
-                   => b
-                   -> ResourceName      -- ^ Resource to save.
+  , save           :: Contents a
+                   => ResourceName      -- ^ Resource to save.
                    -> Author            -- ^ Author of change.
                    -> String            -- ^ Description of change.
                    -> a                 -- ^ New contents of resource.
                    -> IO ()
     
-    -- | Like save, but first verify that the resource name is new.
-    create         :: Contents a
-                   => b
-                   -> ResourceName      -- ^ Resource to create.
-                   -> Author            -- ^ Author of change.
-                   -> String            -- ^ Description of change.
-                   -> a                 -- ^ Contents of resource.
-                   -> IO ()
-
-    -- | Modify a named resource in the filestore.  Like save, except that a revision ID
-    -- must be specified.  If the resource has been modified since the specified revision,
-    -- merge information is returned.  Otherwise, the new contents are saved.  
-    modify         :: Contents a
-                   => b
-                   -> ResourceName      -- ^ Resource to create.
-                   -> RevisionId        -- ^ ID of previous revision that is being modified.
-                   -> Author            -- ^ Author of change.
-                   -> String            -- ^ Description of change.
-                   -> a                 -- ^ Contents of resource.
-                   -> IO (Either MergeInfo ())
-
     -- | Retrieve the contents of the named resource.
-    retrieve       :: Contents a
-                   => b
-                   -> ResourceName      -- ^ Resource to retrieve.
+  , retrieve       :: Contents a
+                   => ResourceName      -- ^ Resource to retrieve.
                    -> Maybe RevisionId  -- ^ @Just@ a particular revision ID, or @Nothing@ for latest
                    -> IO a
 
     -- | Delete a named resource, providing author and log message.
-    delete         :: b
-                   -> ResourceName      -- ^ Resource to delete.
+  , delete         :: ResourceName      -- ^ Resource to delete.
                    -> Author            -- ^ Author of change.
                    -> String            -- ^ Description of change.
                    -> IO ()
 
     -- | Rename a resource, providing author and log message.
-    rename         :: b
-                   -> ResourceName      -- ^ Resource original name.
+  , rename         :: ResourceName      -- ^ Resource original name.
                    -> ResourceName      -- ^ Resource new name.
                    -> Author            -- ^ Author of change.
                    -> String            -- ^ Description of change.
@@ -200,76 +167,64 @@ class FileStore b where
 
     -- | Get history for a list of named resources in a (possibly openended) time range.
     -- If the list is empty, history for all resources will be returned. 
-    history        :: b
-                   -> [ResourceName]    -- ^ List of resources to get history for, or @[]@ for all.
+  , history        :: [ResourceName]    -- ^ List of resources to get history for, or @[]@ for all.
                    -> TimeRange         -- ^ Time range within which to get history.
                    -> IO [Revision]
 
     -- | Return the revision ID of the latest change for a resource.  Raises 'NotFound'
     -- if the resource is not found.
-    latest         :: b
-                   -> ResourceName      -- ^ Resource to get revision ID for.
+  , latest         :: ResourceName      -- ^ Resource to get revision ID for.
                    -> IO RevisionId
 
     -- | Return information about a revision, given the ID.  Raises 'NotFound' if there is
     -- no such revision.
-    revision       :: b
-                   -> RevisionId        -- ^ Revision ID to get revision information for.
+  , revision       :: RevisionId        -- ^ Revision ID to get revision information for.
                    -> IO Revision
 
     -- | Return a list of resources in the filestore.
-    index          :: b
-                   -> IO [ResourceName]
-
-    -- | Return a unified diff of two revisions of a named resource.
-    diff           :: b
-                   -> ResourceName      -- ^ Resource name to get diff for.
-                   -> RevisionId        -- ^ Old revision ID.
-                   -> RevisionId        -- ^ New revision ID.
-                   -> IO String
+  , index          :: IO [ResourceName]
 
     -- | @True@ if the revision IDs match, in the sense that the
     -- can be treated as specifying the same revision.
-    idsMatch       :: b
-                   -> RevisionId
+  , idsMatch       :: RevisionId
                    -> RevisionId
                    -> Bool
 
-    -- Defaults
-
-    modify           = modifyWithMerge
-    create           = genericCreate 
-    diff             = genericDiff
-
-    -- There is sometimes reason to redefine idsMatch -- e.g. in git, where revision IDs
-    -- can be abbreviated, and two IDs match if one is a prefix of the other.
-    idsMatch  _      = (==) 
-
-
--- | An abstract class for a searchable versioning filestore.
-class FileStore b => SearchableFileStore b where
-    search         :: b
-                   -> SearchQuery
+  -- | Search the filestore for patterns. 
+  , search         :: SearchQuery
                    -> IO [SearchMatch]
+
+  }
 
 handleUnknownError :: SomeException -> IO a
 handleUnknownError = throwIO . UnknownError . show
 
--- | A definition of 'create' in terms of 'revision' and 'save'.  Checks to see
--- if the resource already exists: if so, throws a 'ResourceExists' error, if not, saves contents.
-genericCreate :: (Contents a, FileStore b) => b -> ResourceName -> Author -> String -> a -> IO ()
-genericCreate fs name author logMsg contents = catch (latest fs name >> throwIO ResourceExists)
+-- | Like save, but first verify that the resource name is new.  If not, throws a 'ResourceExists'
+-- error.
+create :: Contents a
+       => FileStore
+       -> ResourceName      -- ^ Resource to create.
+       -> Author            -- ^ Author of change.
+       -> String            -- ^ Description of change.
+       -> a                 -- ^ Contents of resource.
+       -> IO ()
+create fs name author logMsg contents = catch (latest fs name >> throwIO ResourceExists)
                                                 (\e -> if e == NotFound
                                                  then save fs name author logMsg contents
                                                  else throwIO e)
 
--- | A definition of 'modify' in terms of 'revision', 'retrieve', and 'save'.  Checks to see if
--- the revision that is being modified is the latest revision of the resource.  If it is,
--- the changes are saved and Right () is returned.  If it is not, a three-way merge is
--- performed and Left (information on the merge) is returned.  It is then up to the calling program
--- to call modify again with the updated revision ID and possibly revised contents.
-modifyWithMerge :: (Contents a, FileStore b) => b -> ResourceName -> RevisionId -> Author -> String -> a -> IO (Either MergeInfo ())
-modifyWithMerge fs name originalRevId author msg contents = do
+-- | Modify a named resource in the filestore.  Like save, except that a revision ID
+-- must be specified.  If the resource has been modified since the specified revision,
+-- @Left@ merge information is returned.  Otherwise, @Right@ the new contents are saved.  
+modify  :: Contents a
+        => FileStore
+        -> ResourceName      -- ^ Resource to create.
+        -> RevisionId        -- ^ ID of previous revision that is being modified.
+        -> Author            -- ^ Author of change.
+        -> String            -- ^ Description of change.
+        -> a                 -- ^ Contents of resource.
+        -> IO (Either MergeInfo ())
+modify fs name originalRevId author msg contents = do
   latestRevId <- latest fs name
   latestRev <- revision fs latestRevId
   if idsMatch fs originalRevId latestRevId
@@ -282,12 +237,14 @@ modifyWithMerge fs name originalRevId author msg contents = do
                                   handleUnknownError
        return $ Left (MergeInfo latestRev conflicts mergedText)
 
--- | A definition of 'diff' in terms of 'retrieve'.  The contents of the two revisions
--- are fetched and run through an external diff program, which produces a unified diff
--- with context lines set to 10,000 (so the whole file will appear, with changes marked
--- by + or - in the left column).
-genericDiff :: FileStore b => b -> ResourceName -> RevisionId -> RevisionId -> IO String
-genericDiff fs name id1 id2 = do
+-- | Return a unified diff of two revisions of a named resource, using an external @diff@
+-- program.
+diff :: FileStore
+     -> ResourceName      -- ^ Resource name to get diff for.
+     -> RevisionId        -- ^ Old revision ID.
+     -> RevisionId        -- ^ New revision ID.
+     -> IO String
+diff fs name id1 id2 = do
   contents1 <- retrieve fs name (Just id1)
   contents2 <- retrieve fs name (Just id2)
   diffContents contents1 contents2
