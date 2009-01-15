0763a7132cdd1b1fba7a38eb5a19feacf28d84ad
jgm <jgm@berkeley.edu>
2009-01-15 15:29:53 -0800

    Changed diff so it takes (Maybe RevisionID); Nothing = latest
    Ignore-this: 9fce0c7d850f2107a559260fbea12e32
    
    darcs-hash:20090115232953-0f649-838e8a8ed80bcd4dd48e1817dd029b13db90eac9.gz

--- Data/FileStore/Generic.hs
+++ Data/FileStore/Generic.hs
@@ -67,11 +67,11 @@ modify fs name originalRevId author msg contents = do
 -- program.
 diff :: FileStore
      -> ResourceName      -- ^ Resource name to get diff for.
-     -> RevisionId        -- ^ Old revision ID.
-     -> RevisionId        -- ^ New revision ID.
+     -> Maybe RevisionId  -- ^ @Just@ old revision ID, or @Nothing@ for latest. 
+     -> Maybe RevisionId  -- ^ @Just@ oew revision ID, or @Nothing@ for latest.
      -> IO String
 diff fs name id1 id2 = do
-  contents1 <- retrieve fs name (Just id1)
-  contents2 <- retrieve fs name (Just id2)
+  contents1 <- retrieve fs name id1
+  contents2 <- retrieve fs name id2
   diffContents contents1 contents2
 
