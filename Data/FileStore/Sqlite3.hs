a2f18394e7b0aa9cdde471b8e83637692990fb9e
jgm <jgm@berkeley.edu>
2009-01-18 14:13:53 -0800

    Added initialize to Sqlite3 module.
    Ignore-this: 4261325d5e0290b9535136082b82cb19
    
    darcs-hash:20090118221353-0f649-d9042907339980a73159a9dda14d413b1e6016fb.gz

--- Data/FileStore/Sqlite3.hs
+++ Data/FileStore/Sqlite3.hs
@@ -35,22 +35,8 @@ import Control.Monad (unless)
 import Text.Regex.Posix ((=~))
 import System.Directory (getPermissions, setPermissions, executable)
 import Database.HDBC.Sqlite3 (Connection(..))
+import Database.HDBC
 import Paths_filestore
-
-{- The sqlite database will contain three tables: pages, revisions, changes.
-
-   - pages will have columns for resource name (string), latest revisionId
-     (int).
-   
-   - revisions will have revisionIds (int unique key), author (string),
-     email (string), change description (string), datetime (datetime).
-
-   - changes will have revisionId (int), code for add/modify/delete (int),
-     resource name (string), contents-after-change (bytestring). 
-
-   This is designed for speed, not space savings.  If you want space
-   efficiency, use git.
--}
 
 -- | Return a filestore implemented using the sqlite3 distributed revision control system
 -- (<http://sqlite3-scm.com/>).
@@ -71,52 +57,54 @@ sqlite3FileStore repo = FileStore {
 
 -- | Initialize a repository, creating the directory if needed.
 sqlite3Init :: Connection -> IO ()
-sqlite3Init repo = undefined
-
--- | Commit changes to a resource.  Raise 'Unchanged' exception if there were
--- no changes.
-sqlite3Commit :: Connection -> [ResourceName] -> Author -> String -> IO ()
-sqlite3Commit repo names author logMsg = undefined
+sqlite3Init conn = do
+  run conn "CREATE TABLE resources(id INTEGER PRIMARY KEY, name TEXT, latestChangeId INTEGER);" []
+  run conn "CREATE TABLE revisions( id INTEGER PRIMARY KEY, author TEXT, email TEXT, description TEXT, datetime DATETIME);" []
+  run conn "CREATE TABLE changes( id INTEGER PRIMARY KEY, revisionId INTEGER, changetype INTEGER, resourceId INTEGER, contents BLOB);" []
+  commit conn
 
 -- | Save changes (creating file and directory if needed), add, and commit.
 sqlite3Save :: Contents a => Connection -> ResourceName -> Author -> String -> a -> IO ()
-sqlite3Save repo name author logMsg contents = undefined
+sqlite3Save conn name author logMsg contents = undefined
+  -- create a revision entry
+  -- create a change entry
+  -- update resources
 
 -- | Retrieve contents from resource.
 sqlite3Retrieve :: Contents a
-            => Connection
-            -> ResourceName
-            -> Maybe RevisionId    -- ^ @Just@ revision ID, or @Nothing@ for latest
-            -> IO a
-sqlite3Retrieve repo name Nothing = undefined
-sqlite3Retrieve repo name (Just revid) = undefined
+                => Connection
+                -> ResourceName
+                -> Maybe RevisionId    -- ^ @Just@ revision ID, or @Nothing@ for latest
+                -> IO a
+sqlite3Retrieve conn name Nothing = undefined
+sqlite3Retrieve conn name (Just revid) = undefined
 
--- | Delete a resource from the repository.
+-- | Delete a resource from the database.
 sqlite3Delete :: Connection -> ResourceName -> Author -> String -> IO ()
-sqlite3Delete repo name author logMsg = undefined
+sqlite3Delete conn name author logMsg = undefined
 
 -- | Change the name of a resource.
 sqlite3Move :: Connection -> ResourceName -> ResourceName -> Author -> String -> IO ()
-sqlite3Move repo oldName newName author logMsg = undefined
+sqlite3Move conn oldName newName author logMsg = undefined
 
 -- | Return revision ID for latest commit for a resource.
 sqlite3LatestRevId :: Connection -> ResourceName -> IO RevisionId
-sqlite3LatestRevId repo name = undefined
+sqlite3LatestRevId conn name = undefined
 
 -- | Get revision information for a particular revision ID, or latest revision.
 sqlite3GetRevision :: Connection -> RevisionId -> IO Revision
-sqlite3GetRevision repo revid = undefined
+sqlite3GetRevision conn revid = undefined
 
--- | Get list of files in repository.
+-- | Get list of files in database.
 sqlite3Index :: Connection -> IO [ResourceName]
-sqlite3Index repo = undefined
+sqlite3Index conn = undefined
 
--- | Uses sqlite3-grep to search repository.
+-- | Uses sqlite3 fts to search database.
 sqlite3Search :: Connection -> SearchQuery -> IO [SearchMatch]
-sqlite3Search repo query = undefined
+sqlite3Search conn query = undefined
 
 -- | Return list of log entries for the given time frame and list of resources.
 -- If list of resources is empty, log entries for all resources are returned.
 sqlite3Log :: Connection -> [ResourceName] -> TimeRange -> IO [Revision]
-sqlite3Log repo names (TimeRange mbSince mbUntil) = undefined
+sqlite3Log conn names (TimeRange mbSince mbUntil) = undefined
 
